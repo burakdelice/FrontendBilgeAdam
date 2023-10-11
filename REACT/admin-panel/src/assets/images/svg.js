26bda720c436297ad48c4378ca11a8b108ad030e
Burak Delice <burakdelice.71@gmail.com>
2023-10-12 00:35:02 +0300

    React Admin Panel

---
+++ REACT/admin-panel/src/assets/images/svg.js
@@ -0,0 +1,48 @@
+export const SunSvg = () => (
+  <svg
+    style={{ enableBackground: "new 0 0 16 16" }}
+    version="1.1"
+    viewBox="0 0 16 16"
+    fill="#fff700"
+  >
+    <circle cx="8.5" cy="7.5" r="4.5" />
+    <rect height="2" width="1" x="8" />
+    <rect height="2" width="1" x="8" y="13" />
+    <rect height="1" width="2" x="14" y="7" />
+    <rect height="1" width="2" x="1" y="7" />
+    <rect
+      height="2"
+      transform="matrix(0.7071 -0.7071 0.7071 0.7071 -4.7175 12.8033)"
+      width="1"
+      x="12.596"
+      y="11.096"
+    />
+    <rect
+      height="2"
+      transform="matrix(0.7071 -0.7071 0.7071 0.7071 -0.9099 3.6109)"
+      width="1"
+      x="3.404"
+      y="1.904"
+    />
+    <rect
+      height="1"
+      transform="matrix(0.7071 -0.7071 0.7071 0.7071 -7.4099 6.3033)"
+      width="2"
+      x="2.904"
+      y="11.596"
+    />
+    <rect
+      height="1"
+      transform="matrix(0.7071 -0.7071 0.7071 0.7071 1.7823 10.1107)"
+      width="2"
+      x="12.096"
+      y="2.404"
+    />
+  </svg>
+);
+
+export const MoonSvg = () => (
+  <svg height="1em" viewBox="0 0 24 24" width="1em">
+    <path fill="#e0e0e0" d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z" />
+  </svg>
+);
